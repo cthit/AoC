9caaef2d06d95708d714fa357b5557dc5230e86b
Mårten Åsberg <marten.asberg@outlook.com>
2021-11-03 22:29:24 +0100

    Gamma authentication

---
+++ src/gamma.rs
@@ -0,0 +1,219 @@
+use std::{
+	error::Error,
+	fmt::{self, Display, Formatter},
+};
+
+use oauth2::{
+	basic::{BasicClient, BasicErrorResponseType},
+	reqwest::async_http_client,
+	AuthUrl,
+	AuthorizationCode,
+	ClientId,
+	ClientSecret,
+	CsrfToken,
+	ErrorResponse,
+	RedirectUrl,
+	RequestTokenError,
+	StandardErrorResponse,
+	TokenResponse,
+	TokenUrl,
+};
+use reqwest::StatusCode;
+use serde::Deserialize;
+
+pub struct GammaClient {
+	oauth_client: BasicClient,
+	api_base: String,
+}
+
+impl GammaClient {
+	pub fn new(
+		client_id: String,
+		client_secret: String,
+		redirect_url: String,
+		callback_url: String,
+		api_base: String,
+	) -> Result<Self, String> {
+		let auth_url = format!("{}/api/oauth/authorize", redirect_url);
+		let token_url = format!("{}/oauth/token", api_base);
+		Ok(Self {
+			oauth_client: BasicClient::new(
+				ClientId::new(client_id),
+				Some(ClientSecret::new(client_secret)),
+				AuthUrl::new(auth_url.clone()).map_err(|_| {
+					format!(
+						"Invalid authorization endpoint URL. (Attempt: \"{}\")",
+						auth_url
+					)
+				})?,
+				Some(TokenUrl::new(token_url.clone()).map_err(|_| {
+					format!("Invalid token endpoint URL. (Attempt: \"{}\")", token_url)
+				})?),
+			)
+			.set_redirect_uri(
+				RedirectUrl::new(callback_url.clone()).map_err(|_| {
+					format!("Invalid redirect URL. (Attempt: \"{}\")", callback_url)
+				})?,
+			),
+			api_base,
+		})
+	}
+
+	pub fn authorize_url(&self, back: String) -> String {
+		let auth_url = self.oauth_client.authorize_url(|| CsrfToken::new(back));
+		let (url, _) = auth_url.url();
+		url.as_str().to_string()
+	}
+
+	pub async fn get_token(
+		&self,
+		code: String,
+	) -> Result<
+		String,
+		GammaError<
+			!,
+			oauth2::reqwest::Error<reqwest::Error>,
+			RequestTokenError<
+				oauth2::reqwest::Error<reqwest::Error>,
+				StandardErrorResponse<BasicErrorResponseType>,
+			>,
+			RequestTokenError<
+				oauth2::reqwest::Error<reqwest::Error>,
+				StandardErrorResponse<BasicErrorResponseType>,
+			>,
+		>,
+	> {
+		let token_response = self
+			.oauth_client
+			.exchange_code(AuthorizationCode::new(code))
+			.request_async(async_http_client)
+			.await
+			.map_err(GammaError::from)?;
+		Ok(token_response.access_token().secret().clone())
+	}
+
+	pub async fn get_user(
+		&self,
+		token: String,
+	) -> Result<ITUser, GammaError<reqwest::Error, reqwest::Error, reqwest::Error, reqwest::Error>>
+	{
+		reqwest::Client::new()
+			.get(&format!("{}/users/me", self.api_base))
+			.header("Authorization", format!("Bearer {}", token))
+			.send()
+			.await
+			.map_err(GammaError::from)?
+			.json::<ITUser>()
+			.await
+			.map_err(GammaError::from)
+	}
+}
+
+#[derive(Debug)]
+pub enum GammaError<
+	TA: Error + 'static,
+	TB: Error + 'static,
+	TC: Error + 'static,
+	TD: Error + 'static,
+> {
+	Unauthorized(TA),
+	RequestError(TB),
+	ClientError(TC),
+	UnknownError(TD),
+}
+
+impl From<reqwest::Error>
+	for GammaError<reqwest::Error, reqwest::Error, reqwest::Error, reqwest::Error>
+{
+	fn from(err: reqwest::Error) -> Self {
+		if let Some(status) = err.status() {
+			if status == StatusCode::UNAUTHORIZED {
+				GammaError::Unauthorized(err)
+			} else {
+				GammaError::RequestError(err)
+			}
+		} else if err.is_decode() {
+			GammaError::ClientError(err)
+		} else {
+			GammaError::UnknownError(err)
+		}
+	}
+}
+
+impl<TE: Error + 'static, TR: ErrorResponse> From<RequestTokenError<TE, TR>>
+	for GammaError<!, TE, RequestTokenError<TE, TR>, RequestTokenError<TE, TR>>
+{
+	fn from(err: RequestTokenError<TE, TR>) -> Self {
+		match err {
+			RequestTokenError::ServerResponse(_) => GammaError::UnknownError(err),
+			RequestTokenError::Request(err) => GammaError::RequestError(err),
+			RequestTokenError::Parse(_, _) => GammaError::ClientError(err),
+			RequestTokenError::Other(_) => GammaError::UnknownError(err),
+		}
+	}
+}
+
+impl<TA: Error + 'static, TB: Error + 'static, TC: Error + 'static, TD: Error + 'static> Error
+	for GammaError<TA, TB, TC, TD>
+{
+	fn source(&self) -> Option<&(dyn Error + 'static)> {
+		Some(match self {
+			GammaError::Unauthorized(err) => err,
+			GammaError::RequestError(err) => err,
+			GammaError::ClientError(err) => err,
+			GammaError::UnknownError(err) => err,
+		})
+	}
+}
+
+impl<TA: Error, TB: Error, TC: Error, TD: Error> Display for GammaError<TA, TB, TC, TD> {
+	fn fmt(&self, f: &mut Formatter) -> Result<(), fmt::Error> {
+		match self {
+			GammaError::Unauthorized(_) => write!(f, "Unauthorized"),
+			GammaError::RequestError(_) => write!(f, "Request error"),
+			GammaError::ClientError(_) => write!(f, "Client error"),
+			GammaError::UnknownError(err) => write!(f, "Unknown error ({})", err),
+		}
+	}
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ITUser {
+	pub id: String,
+	pub cid: String,
+	pub nick: String,
+	pub avatar_url: String,
+	pub acceptance_year: u16,
+	pub language: String,
+	pub groups: Option<Vec<FKITGroup>>,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct FKITGroup {
+	pub id: String,
+	pub becomes_active: u64,
+	pub becomes_inactive: u64,
+	pub name: String,
+	pub pretty_name: String,
+	pub avatar_url: Option<String>,
+	pub super_group: FKITSuperGroup,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct FKITSuperGroup {
+	pub id: String,
+	pub name: String,
+	pub pretty_name: String,
+	pub r#type: GroupType,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "UPPERCASE")]
+pub enum GroupType {
+	Committee,
+	Society,
+	Alumni,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,90 @@
 #![feature(never_type, proc_macro_hygiene, decl_macro)]
 
 mod db;
+mod gamma;
 
 #[macro_use]
 extern crate diesel;
 #[macro_use]
 extern crate rocket;
 
+use std::env;
+
 use db::DbConn;
-use rocket::{fairing::AdHoc, Build, Rocket};
+use gamma::GammaClient;
+use lazy_static::lazy_static;
+use rocket::{
+	fairing::AdHoc,
+	http::{Cookie, CookieJar},
+	response::Redirect,
+	Build,
+	Rocket,
+};
+
+static GAMMA_COOKIE: &str = "gamma";
+lazy_static! {
+	static ref GAMMA_CLIENT_ID: String =
+		env::var("GAMMA_CLIENT_ID").expect("Missing the GAMMA_CLIENT_ID environment variable.");
+	static ref GAMMA_CLIENT_SECRET: String = env::var("GAMMA_CLIENT_SECRET")
+		.expect("Missing the GAMMA_CLIENT_SECRET environment variable.");
+	static ref GAMMA_REDIRECT_URL: String = env::var("GAMMA_REDIRECT_URL")
+		.expect("Missing the GAMMA_REDIRECT_URL environment variable.");
+	static ref CALLBACK_URL: String =
+		env::var("CALLBACK_URL").expect("Missing the CALLBACK_URL environment variable.");
+	static ref GAMMA_URL: String = format!(
+		"{}/api",
+		env::var("GAMMA_URL").expect("Missing the GAMMA_URL environment variable.")
+	);
+	static ref OAUTH_CLIENT: GammaClient = GammaClient::new(
+		GAMMA_CLIENT_ID.to_string(),
+		GAMMA_CLIENT_SECRET.to_string(),
+		GAMMA_REDIRECT_URL.to_string(),
+		CALLBACK_URL.to_string(),
+		GAMMA_URL.to_string(),
+	)
+	.unwrap_or_else(|e| panic!("Failed to create the OAuth client. {}", e));
+}
+
+#[get("/login?<back>")]
+async fn login(back: Option<String>, cookies: &CookieJar<'_>) -> Redirect {
+	if let Some(access_cookie) = cookies.get(GAMMA_COOKIE) {
+		if OAUTH_CLIENT
+			.get_user(access_cookie.value().to_string())
+			.await
+			.is_ok()
+		{
+			return Redirect::to(back.unwrap_or_else(|| "/".to_string()));
+		}
+	}
+	Redirect::to(OAUTH_CLIENT.authorize_url(back.unwrap_or_else(|| "/".to_string())))
+}
+
+#[get("/callback?<code>&<state>")]
+async fn callback(code: String, state: Option<String>, cookies: &CookieJar<'_>) -> Redirect {
+	match OAUTH_CLIENT.get_token(code).await {
+		Ok(access_token) => match OAUTH_CLIENT.get_user(access_token.clone()).await {
+			Ok(_) => {
+				cookies.add(Cookie::build(GAMMA_COOKIE, access_token).path("/").finish());
+				Redirect::to(state.unwrap_or_else(|| "/".to_string()))
+			}
+			Err(err) => Redirect::to(uri!(unauthorized(Some(err.to_string())))),
+		},
+		Err(err) => Redirect::to(uri!(unauthorized(Some(err.to_string())))),
+	}
+}
+
+#[get("/callback?<error>&<error_description>", rank = 2)]
+async fn callback_error(error: String, error_description: Option<String>) -> String {
+	match error_description {
+		Some(desc) => format!("{}\n{}", error, desc),
+		None => error,
+	}
+}
+
+#[get("/unauthorized?<reason>")]
+fn unauthorized(reason: Option<String>) -> String {
+	reason.unwrap_or_else(|| "Unauthorized".to_string())
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -30,5 +106,11 @@ fn rocket() -> Rocket<Build> {
 				}
 			})
 		}))
-		.mount("/", routes![index])
+		.mount("/", routes![
+			index,
+			login,
+			callback,
+			callback_error,
+			unauthorized,
+		])
 }
